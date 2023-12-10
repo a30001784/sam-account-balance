0d06b44b69e9dc51afb75f122597959d2e6040ce
Feng Wu <50124330+a30001784@users.noreply.github.com>
2023-12-10 17:23:39 +1100

    add new python modules

--- account_balance/app.py
+++ account_balance/app.py
@@ -1,34 +1,35 @@
 import requests
 import smtplib
-import time
-import json
-import configparser
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from datetime import datetime
 from forex_python.converter import CurrencyRates
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup as bs
 
 
 # Example usage
 symbols = ['ONEUSDT', 'DOGEUSDT']
-holding_quantities = [346998, 121266]            # as of 01/12/2023
+holding_quantities = [346998, 121266]             # as of 20/8/2023
 initial_account_balance = 40000
-CONFIG_FILE = './config.ini'
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE)
+us_stock_position = 4938
+us_stock_ticker = 'open'
+us_stock_metric = 'Price'
+cash = 13453
 
 def fundamental_metric(soup, metric):
     return soup.find(text = metric).find_next(class_='snapshot-td2').text
 
-def exchange_usd_to_aud(usd_amount):
+
+def get_fundamental_data(ticker, metric):
     try:
-        c = CurrencyRates()
-        aud_amount = c.convert('USD', 'AUD', usd_amount)
-        return aud_amount
-    
+        url = ("http://finviz.com/quote.ashx?t=" + ticker.lower())
+        session = HTMLSession()
+        response = session.get(url)
+        soup =bs(response.content, 'html.parser')
+        price = fundamental_metric(soup,metric)
     except Exception as e:
-    # If there's an error, return the error message
-        return str(e)
+        print(ticker, 'not found')
+    return float(price)
 
 def get_binance_market_data(symbol):
     url = f'https://api.binance.com/api/v3/ticker/price?symbol={symbol}'
@@ -44,18 +45,19 @@ def calculate_account_balance(symbols, holding_quantities):
         account_balance += price * holding_quantity
     return account_balance
 
-def read_config(file_path='config.json'):
-    try:
-        with open(file_path, 'r') as file:
-            config = json.load(file)
+def get_stock_account_balance(holding_size, ticker, metric):
+    current_price = get_fundamental_data(ticker, metric)
 
-        return config
-    except FileNotFoundError:
-        print(f'Error: Config file "{file_path}" not found.')
-        return None
-    except json.JSONDecodeError as e:
-        print(f'Error decoding config file: {e}')
-        return None
+    # Calculate the stock account balance
+    account_balance = current_price * holding_size
+    # print(f"The current stock account balance for {ticker} is: ${account_balance}")
+    return account_balance
+
+def exchange_usd_to_aud(usd_amount):
+    c = CurrencyRates()
+    aud_amount = c.convert('USD', 'AUD', usd_amount)
+    return aud_amount
+
 
 def send_email(subject, body):
     from_email = '156709406@qq.com'
@@ -69,10 +71,10 @@ def send_email(subject, body):
     message['From'] = from_email
     message['To'] = to_email
     message['Subject'] = subject
-    
+
     # Attach the body of the email
     message.attach(MIMEText(body, 'plain'))
-    
+
     print(f'Start to send email')
 
     try:
@@ -80,12 +82,12 @@ def send_email(subject, body):
             # Login to the email server
             server.starttls()
             server.login(smtp_username, smtp_password)
-            
+
             # Send the email
             print(f'Start to send the message')
             server.sendmail(from_email, to_email, message.as_string())
             server.quit()
-                # If successful, return 0
+            # If successful, return 0
 
         # If successful, return 0
         return 0
@@ -94,60 +96,62 @@ def send_email(subject, body):
         # If there's an error, return the error message
         return str(e)
 
+
 def cmd_email():
+
+    values = [cash]
     #  Calculate daily account balance
-    subject = 'Daily Bitcoin Account Balance'
+    subject = 'Daily Account Balance'
+    us_stock_balance = get_stock_account_balance(us_stock_position, us_stock_ticker, us_stock_metric)
+    us_stock_balance_aud = exchange_usd_to_aud(us_stock_balance)
+
     bitcoin_account_balance = calculate_account_balance(symbols, holding_quantities)
     bitcoin_account_balance_aud = exchange_usd_to_aud(bitcoin_account_balance)
+    values.append(bitcoin_account_balance_aud)
 
-    body1 = f'Daily Account Balance: {bitcoin_account_balance:.2f} USDT'
-    body2 = f'Daily Account Balance: AU${bitcoin_account_balance_aud:.2f}'
-    body = body1 + '\n' + body2
-    
+    print(f'Total US Stock Account Balance: US${us_stock_balance:.2f}')
+    print(f'Total US Stock Account Balance: AU${us_stock_balance_aud:.2f}')
+
+    print(f'Daily Bitcoin Account Balance: {bitcoin_account_balance:.2f} USDT')
+    print(f'Daily Bitcoin Account Balance: AU${bitcoin_account_balance_aud:.2f}')
+
+    #### Account balance for US Stock ##########
+    body1 = f'US Stock Balance: US${us_stock_balance:.2f}'
+    body2 = f'US Stock Balance: AU${us_stock_balance_aud:.2f}'
+
+    #### Account balance for bitcoin ##########
+    body3 = f'Bitcoin Balance: {bitcoin_account_balance:.2f} USDT'
+    body4 = f'Bitcoin Balance: AU${bitcoin_account_balance_aud:.2f}'
+
+    ##### Total Account Balance ############
+    values.append(us_stock_balance_aud)
+    total_balance = sum(values)
+    body5 = f'Total Balance: AU${total_balance:.2f}'
+
+    body = body1 + '\n' + body2 + '\n' + body3 + '\n' + body4 + '\n' + body5
+
     # Send the email on the bitcoin account balance
     result = send_email(subject, body)
-    
+
     if result == 0:
         print('Email sent successfully.')
     else:
         print(f'Error sending email: {result}')
     return result
 
-def cmd_ustock():
-    config = read_config()
-    if config is None:
-        return
+def main():
 
-    # Accessing ticker and position from the config file
-    ticker = config.get('ticker')
-    position = config.get('position')
-
-    # Pretty-printing the JSON config
-    print('Config:')
-    print(json.dumps(config, indent=2))
-
-    print(f'Ticker: {ticker}, Position: {position}')
-
-def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
     cmd_email()
-    
+
+    # Save the result of daily account balance to CSV file
+    # save_daily_account_balance_to_csv(daily_balance)
+
+    # Plot the account balance
+    # csv_file = 'daily_account_balances.csv'
+    # dates, balances = read_daily_account_balance_from_csv(csv_file)
+    # draw_daily_account_balance(dates, balances)
+
+if __name__ == "__main__":
+    main()
+
+
