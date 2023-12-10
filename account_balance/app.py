fa136f4740e515732bfab5331c32823cb931bb62
Feng Wu <50124330+a30001784@users.noreply.github.com>
2023-12-10 13:58:56 +1100

    update lambda function

--- account_balance/app.py
+++ account_balance/app.py
@@ -1,22 +1,29 @@
 import requests
 import smtplib
+import time
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from datetime import datetime
 from forex_python.converter import CurrencyRates
 
 
 # Example usage
 symbols = ['ONEUSDT', 'DOGEUSDT']
-holding_quantities = [346998, 121266]          # as of 20/8/2023
+holding_quantities = [346998, 121266]            # as of 28/06/2023
 initial_account_balance = 40000
 
 def fundamental_metric(soup, metric):
     return soup.find(text = metric).find_next(class_='snapshot-td2').text
 
 def exchange_usd_to_aud(usd_amount):
-    c = CurrencyRates()
-    aud_amount = c.convert('USD', 'AUD', usd_amount)
-    return aud_amount
+    try:
+        c = CurrencyRates()
+        aud_amount = c.convert('USD', 'AUD', usd_amount)
+        return aud_amount
+    
+    except Exception as e:
+    # If there's an error, return the error message
+        return str(e)
 
 def get_binance_market_data(symbol):
     url = f'https://api.binance.com/api/v3/ticker/price?symbol={symbol}'
@@ -32,23 +39,42 @@ def calculate_account_balance(symbols, holding_quantities):
         account_balance += price * holding_quantity
     return account_balance
 
-def send_email(to_email, subject, body):
+def send_email(subject, body):
     from_email = '156709406@qq.com'
+    to_email = '156709406@qq.com'
     smtp_server = 'smtp.qq.com'
     smtp_port = 587
     smtp_username = '156709406@qq.com'
     smtp_password = 'esdrnpzdsscbcajj'
 
-    msg = MIMEText(body)
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
+    message = MIMEMultipart()
+    message['From'] = from_email
+    message['To'] = to_email
+    message['Subject'] = subject
+    
+    # Attach the body of the email
+    message.attach(MIMEText(body, 'plain'))
+    
+    print(f'Start to send email')
 
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(smtp_username, smtp_password)
-    server.send_message(msg)
-    server.quit()
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            # Login to the email server
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            
+            # Send the email
+            print(f'Start to send the message')
+            server.sendmail(from_email, to_email, message.as_string())
+            server.quit()
+                # If successful, return 0
+
+        # If successful, return 0
+        return 0
+
+    except Exception as e:
+        # If there's an error, return the error message
+        return str(e)
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -71,36 +97,16 @@ def lambda_handler(event, context):
 
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
-
-    try:
-        #  Calculate daily account balance
-        bitcoin_account_balance = calculate_account_balance(symbols, holding_quantities)
-        bitcoin_account_balance_aud = exchange_usd_to_aud(bitcoin_account_balance)
-
-        # Print daily account balance
-        print(f'Daily Bitcoin Account Balance: {bitcoin_account_balance:.2f} USDT')
-        print(f'Daily Bitcoin Account Balance: AU${bitcoin_account_balance_aud:.2f}')
-
-        # Send email with the result
-        to_email = '156709406@qq.com'
-        subject = 'Daily Bitcoin Account Balance'
-        body1 = f'Daily Account Balance: {bitcoin_account_balance:.2f} USDT'
-        body2 = f'Daily Account Balance: AU${bitcoin_account_balance_aud:.2f}'
-        body = body1 + '\n' + body2
-        send_email(to_email, subject, body)
-
-        return {
-            'statusCode': 200,
-            'body': 'Daily Account Balance calculated and email sent'
-        }
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        raise
-
-    # return {
-    #     "statusCode": 200,
-    #     "body": json.dumps({
-    #         "message": "hello world",
-    #         # "location": ip.text.replace("\n", "")
-    #     }),
-    # }
+#  Calculate daily account balance
+    subject = 'Daily Bitcoin Account Balance'
+    bitcoin_account_balance = calculate_account_balance(symbols, holding_quantities)
+    body1 = f'Daily Account Balance: {bitcoin_account_balance:.2f} USDT'
+    
+    # Send the email on the bitcoin account balance
+    result = send_email(subject, body1)
+    
+    if result == 0:
+        print('Email sent successfully.')
+    else:
+        print(f'Error sending email: {result}')
+    
